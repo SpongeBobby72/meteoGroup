4cd05f2dc0dfa1097f4cb1caf6e1bc87f06ee51f
kevinhueri <kevin.hueri@gmail.com>
2021-11-10 16:53:50 +0100

    changement du button inputTown via MUI

--- src/components/InputTown.js
+++ src/components/InputTown.js
@@ -1,20 +1,21 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 import { store } from "../store";
 
 import { getWeather, getWeathersWeek } from "../store/actions/WeatherActions";
 
 const InputTown = (props) => {
-
   function getValue() {
     var input = document.getElementById("ville").value;
     if (input.length > 0) {
       store.dispatch(getWeather(input));
       store.dispatch(getWeathersWeek(input));
     }
-}
+  }
 
   return (
     <div className="InputTown">
@@ -26,12 +27,12 @@ const InputTown = (props) => {
         noValidate
         autoComplete="off"
       >
-        <TextField
-          id="ville"
-          label="Ville"
-          variant="outlined"
-        />
-        <input type="button" value="Click me" onClick={(e) => getValue(e)} />
+        <TextField id="ville" label="Ville" variant="outlined" />>
+        <Stack spacing={2} direction="row">
+          <Button 
+          onClick={(e) => getValue(e)}
+          variant="contained">OK</Button>
+        </Stack>
       </Box>
     </div>
   );
