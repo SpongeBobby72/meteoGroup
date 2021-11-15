ee7d973964ccde8ecbaf9f52ac1e72842467c896
cda3 <massot.etiennepro@gmail.com>
2021-11-15 15:32:52 +0100

    Add-folder-Mui

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -7,14 +7,8 @@ import * as Mui from '@mui/material';
 const Buttons = () => {
   return (
     <div>
-<<<<<<< HEAD
-      {/* <Cards/> */}
-      <Stack direction="row" spacing={2}>
-        <Button href="/articles#/article" variant="contained" color="warning">
-=======
       <Mui.Stack direction="row" spacing={2}>
         <Mui.Button href="/articles#/article" variant="contained" color="warning">
->>>>>>> 05da7c96ca90f92b08b8fbd9baa8fc07ebee8446
           ARTICLE
         </Mui.Button>
       </Mui.Stack>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,5 @@
 import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
 import React from "react";
-<<<<<<< HEAD
-=======
-// import { NavLink } from "react-router-dom";
-import Buttons from "../components/Buttons";
->>>>>>> 05da7c96ca90f92b08b8fbd9baa8fc07ebee8446
 import InputTown from "./InputTown";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Buttons from "../components/Buttons";
@@ -20,13 +15,7 @@ const NavBar = () => {
         ></AccordionSummary>
         <AccordionDetails>
           <InputTown />
-<<<<<<< HEAD
             <Buttons /> 
-=======
-          {/* <NavLink exact to="/article" activeClassname="nav-active"> */}
-            <Buttons />
-          {/* </NavLink> */}
->>>>>>> 05da7c96ca90f92b08b8fbd9baa8fc07ebee8446
         </AccordionDetails>
       </Accordion>
     </div>
