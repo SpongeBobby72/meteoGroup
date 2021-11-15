c53e3b5f088a019b8e27aeea64f0c2adae211aaf
wilfried-72 <wilfried.zanni@outlook.fr>
2021-11-15 16:48:36 +0100

    mise en forme dossier utils

--- src/components/ForeCast.js
+++ src/components/ForeCast.js
@@ -1,15 +1,6 @@
 import * as React from "react";
-<<<<<<< HEAD
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { ArrowCircleUp } from "@mui/icons-material";
-import fn from "../utils";
-=======
 // MUI
 import * as theme from "../theme";
->>>>>>> dev
 
 const ForeCast = (data) => {
   // const {directionWin, dateFormatMeteo } = require("../util");
@@ -19,15 +10,6 @@ const ForeCast = (data) => {
   // const func = require ("../utils")
 
   return (
-<<<<<<< HEAD
-    <div className="cardDiv">
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia className={data.daily.weather[0].main} />
-        <CardContent>
-          <Typography sx={{ color: "white" }} component="p">
-            {/* {fn.dateFormatMeteo(data.daily.dt)} */}
-          </Typography>
-=======
     <div className="cardDiv">     
       <theme.Card sx={{ maxWidth: 345 }}>
         <theme.CardMedia className={data.daily.weather[0].main} />
@@ -35,7 +17,6 @@ const ForeCast = (data) => {
           <theme.Typography sx={{ color: "white" }} component="p">
             {dateFormatMeteo(data.daily.dt)}
           </theme.Typography>
->>>>>>> dev
 
           <theme.Typography sx={{ color: "white" }} component="div">
             {data.daily.weather[0].description}
@@ -48,30 +29,9 @@ const ForeCast = (data) => {
             component="div"
           >
             {Math.round(data.daily.temp.day)} °C
-<<<<<<< HEAD
-          </Typography>
-
-          <Typography sx={{ mr: 1, color: "white" }} component="span">
-            {Math.round(data.daily.wind_speed)} km/h
-          </Typography>
-
-          <Typography sx={{ color: "white" }} component="span">
-            {/* ( {fn.directionWin(data.daily.wind_deg)} ) */}
-          </Typography>
-
-          <ArrowCircleUp
-            sx={{
-              color: "white",
-              transform: `rotateZ(${data.daily.wind_deg}deg)`,
-            }}
-          />
-        </CardContent>
-      </Card>
-=======
           </theme.Typography>
         </theme.CardContent>
       </theme.Card>
->>>>>>> dev
     </div>
   );
 };
--- src/components/RainCard.js
+++ src/components/RainCard.js
@@ -2,16 +2,9 @@
 // ******* COMPONENT PARENT DE MeteoRainHour.js *******
 //         *************************************
 import * as React from "react";
-<<<<<<< HEAD
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import {Datee} from "../utils/index";
-=======
 
  //import component Mui
 import * as theme from "../theme";
->>>>>>> dev
 
 const RainCard = (data) => {
   // const { minParser } = require("../util/index");
@@ -23,17 +16,10 @@ const RainCard = (data) => {
         
       <theme.Card sx={{ width: 100 ,backgroundColor: 'transparent'}}>
       
-<<<<<<< HEAD
-        <CardContent> 
-          <Typography  component="p" sx={{color:"white"}}>
-          {Datee.minParser(data.minutely.dt)}
-          </Typography>
-=======
         <theme.CardContent> 
           <theme.Typography  component="p" sx={{color:"white"}}>
           {MinParser(data.minutely.dt)}
           </theme.Typography>
->>>>>>> dev
 
           <theme.Typography component="p" sx={{color:"white"}}>
           {data.minutely.precipitation} % 
